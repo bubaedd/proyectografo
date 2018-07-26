e8d7d7fc16627878483d386ee1312e4ad2282c74
bubaedd <bvargas@icci.cl>
2018-07-26 17:21:50 -0400

    primer commit del proyecto y espero que el ultimo

---
+++ src/grafoconexo/Grafoconexo.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafoconexo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author buba
+ */
+public class Grafoconexo {
+  public static Scanner cin = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Menu");
+        System.out.print("\n");
+        int elegir=3;
+        while(elegir != 0){
+            System.out.println("0.terminar programa");
+            System.out.println("1.empezar programa");
+            elegir = cin.nextInt();
+        if(elegir == 1){
+        
+        
+        System.out.println("ingrese numero de vertices:");
+        int n = cin.nextInt();
+        int [][] m = new int[n][n];
+        
+        
+         System.out.println("Ingresar datos a matriz de adyacencia");
+         for(int i = 0; i<m.length;i++){
+             for(int j=0; j<m.length;j++){
+                 System.out.print("ingrese adyacencia de: [" + i + "][" + j + "]: ");
+                 m[i][j] = cin.nextInt();
+             }            
+         }
+         
+          System.out.print("\n");
+         System.out.println("Matriz de adyacencia:");
+          imprimir(m);
+          
+          int [][] mult=m;
+          System.out.print("\n");
+          System.out.println("ingresar numeros de caminos a buscar:");
+          int caminos = cin.nextInt();
+          if(caminos == 0){
+              mult=midentidad(n);
+          }
+          else{
+          for(int i = 0 ; i<caminos ; i++){
+              if(i == 0){
+                  mult=m;
+              }
+              else if(i==1){
+                  mult = multiplicar(m,m);
+              }
+              else if(i >= 2){
+                  mult = multiplicar(mult,m);
+              }
+          }
+          }
+          
+         System.out.println("cantidad de caminos entre cada vertice:");
+         int contador =0;
+         imprimir(mult);
+         for(int i=0; i<mult.length;i++){
+             for(int j=0; j<mult.length;j++){
+                 if(mult[i][j]==0){
+                 contador++;
+                 }
+             }
+         }
+         
+         if(contador == 0){
+             System.out.println("el grafo es conexo");
+         }
+         else{
+             System.out.println("el grafo no es conexo");
+             
+         }
+        }
+        else if(elegir == 0){
+            System.out.println("adiosito");
+            break;
+        }
+           
+        } 
+        
+    }
+    
+    public static int[][] multiplicar (int[][] m1,int[][] m2) {
+		
+		int fil_m1 = m1.length;
+		int col_m1 = m1[0].length;
+		
+		int fil_m2 = m2.length;
+		int col_m2 = m2[0].length;
+		
+		if (col_m1 != fil_m2)
+			throw new RuntimeException("No se pueden multiplicar las matrices");
+		
+		// La nueva matriz es de filas de M1 y columnas de M2
+		int[][] multiplicacion = new int[fil_m1][col_m2];
+		
+		for (int x=0; x < multiplicacion.length; x++) {
+            for (int y=0; y < multiplicacion[x].length; y++) {
+            		
+            		// El nuevo bucle suma la multiplicaci�n de la fila por la columna
+            		for (int z=0; z<col_m1; z++) {
+            			multiplicacion [x][y] += m1[x][z]*m2[z][y];
+            		}
+            }
+		}
+		
+		return multiplicacion;
+		
+	}
+    
+    public static void imprimir(int[][] matriz) {
+		for (int x=0; x < matriz.length; x++) {
+            System.out.print("\n");
+            for (int y=0; y < matriz[x].length; y++) {
+                    System.out.print (matriz[x][y]);
+                    if (y!=matriz[x].length-1) System.out.print("\t");
+            }
+            System.out.print("\n");
+		}
+	}
+    
+     public static int[][] midentidad (int n){
+         int [][]m = new int [n][n];
+         for(int i =0; i<m.length;i++){
+             for(int j=0;j<m.length;j++){
+                 if(i==j){
+                     m[i][j]=1;
+                 }
+                 else{
+                     m[i][j]=0;
+                 }
+             }
+         }
+         
+         return m;
+     }
+    
+    
+    
+}
